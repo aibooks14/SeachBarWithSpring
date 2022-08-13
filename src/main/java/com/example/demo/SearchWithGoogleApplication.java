03dff504d9dbbb74a31758e40e467a085e5e6936
aibooks14 <101002119+aibooks14@users.noreply.github.com>
2022-08-13 15:16:19 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/SearchWithGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchWithGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchWithGoogleApplication.class, args);
+	}
+
+}
